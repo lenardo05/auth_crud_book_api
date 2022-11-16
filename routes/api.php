294639725d1f0e3edeccfc9aa69b4791697041fa
Leonardo Costa <lenardo05@gmail.com>
2022-11-16 10:11:07 -0300

    Create routes book

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/books', [BookStoreController::class, 'index']);
+Route::get('/books/{id}', [BookStoreController::class, 'show']);
+
 Route::group([
     'prefix' => 'auth'
 ], function () {
