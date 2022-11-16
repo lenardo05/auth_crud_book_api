811075ce9ec06f75581313ec8a0af06833e5243a
Leonardo Costa <lenardo05@gmail.com>
2022-11-16 09:41:16 -0300

    Create signup and login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,85 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\UserSignupRequest;
+use App\Http\Requests\UserLoginRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\User;
 
 class AuthController extends Controller
 {
-    //
+    /**
+     * Create user
+     *
+     * @param  [string] name
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [string] password_confirmation
+     * @return [string] message
+     */
+    public function signup(UserSignupRequest $request)
+    {
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            'success'   => true,
+            'message'   => 'Successfully created user',
+            'data'      => $user,
+            'code'      => 201
+        ], 201);  
+    }
+
+    /**
+     * Login user and create token
+     *
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [boolean] remember_me
+     * @return [string] access_token
+     * @return [string] token_type
+     * @return [string] expires_at
+     */
+    public function login(UserLoginRequest $request)
+    {
+        $credentials = request(['email', 'password']);
+
+        if(!Auth::attempt($credentials))
+            return response()->json([
+                'success'   => false,
+                'message'   => 'Unauthorized',
+                'data'      => $credentials,
+                'code'      => 401
+            ], 401);  
+
+        $user = $request->user();
+
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+
+        if ($request->remember_me)
+            $token->expires_at = Carbon::now()->addWeeks(1);
+
+        $token->save();
+
+        return response()->json([
+            'success'   => true,
+            'message'   => 'Login successfully',
+            'access_token' => $tokenResult->accessToken,
+            'token_type'   => 'Bearer',
+            'expires_at'   => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString(),
+            'code'         => 201
+
+        ]);
+
+    }
 }
